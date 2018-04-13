4c7a917fe89ed388c0f9f5838d095e030a6f903d
Aysyluu <ais.malygina@yandex.ru>
2018-04-13 07:46:42 -0700

    added tests

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![Build Status](https://travis-ci.org/Aysyluu/lab05.svg?branch=master)](https://travis-ci.org/Aysyluu/lab05)
+[![Build Status](https://travis-ci.org/Aysyluu/lab06.svg?branch=master)](https://travis-ci.org/Aysyluu/lab06)
---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aysylu/Aysyluu/workspace/projects/lab06
+# Build directory: /home/aysylu/Aysyluu/workspace/projects/lab06/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/aysylu/Aysyluu/workspace/projects/lab06/_build/check" "-s" "-r" "compact" "--use-colour" "yes")
